0605f2ad2724f80ac4e46cb4d8776ce26791a6f1
Igor-Gorchakov <Igor_Gorchakov@epam.com>
2023-10-09 21:37:16 +0300

    Restructured the project, added new modules for consumer and producer

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 https://sehun.me/kafka-cluster-with-multiple-brokers-97623034df0b
+
 https://howtodoinjava.com/kafka/kafka-cluster-setup-using-docker-compose/
---
+++ producer/src/main/java/com/example/producer/KafkaProducer.java
@@ -0,0 +1,19 @@
+package com.example.producer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class KafkaProducer {
+    private final static String TOPIC = "test-topic";
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void send(Object payload) {
+        log.info("Sending payload='{}' to topic='{}'", payload, TOPIC);
+        kafkaTemplate.send(TOPIC, payload);
+    }
+}
